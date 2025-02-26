b2f9b5b1d69fd9721ccc83cd0a83e7041efbf423
Grzegorz Kruk <krukg02@gmail.com>
2025-02-26 17:01:10 +0100

    Najmniejsza brakująca liczba
    Użytkownik podaje liczby, program podaje najmniejsza brakującą liczbę

---
+++ Pierwsza brakujaca liczba/Pierwsza brakujaca liczba/Pierwsza brakujaca liczba.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+void fliczby(int &nliczb, set<int> &s1);
+void fporownaj(int &nliczb, set<int> &s1, set<int>& s2, set <int> &brak);
+
+int main() {
+	set <int> s1,s2; // s1 to liczby 1-nlibcz, s2 to liczby uzytkownika
+	set <int> brak; // brakujace liczby 
+	int nliczb = 0;
+	cout << "Podaj ilosc elementow: ";
+	cin >> nliczb;
+	fliczby(nliczb, s1);
+
+	// Pobieranie liczb od uzytkownika
+	int bufor2;
+	for (int i = 0; i < nliczb; i++) {
+		cout << "(" << i + 1 << " z " << nliczb << "): ";
+		cin >> bufor2;
+		s2.insert(bufor2);
+	}
+	fporownaj(nliczb, s1, s2, brak);
+
+	for (int x : brak) {
+		cout << "Najmniejsza brakujaca liczba : " << x << " ";
+	}
+
+	return 0;
+}
+
+void fliczby(int &nliczb, set<int> &s1){
+	for (int i = 0; i < nliczb;i++) {
+		int bufor = i + 1;
+		s1.insert(bufor);
+	}
+}
+void fporownaj(int& nliczb, set<int>& s1, set<int>& s2, set <int> &brak){
+	for (int x : s1) {
+		if (!s2.count(x)) { 
+			brak.insert(x); 
+			break; 
+		}
+	
+	}
+}
